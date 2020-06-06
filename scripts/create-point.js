8107bea3192ac3f110aa8a0b6919fc0670228ca7
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-06-06 18:09:22 +0200

    Created const to cities

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -24,7 +24,16 @@ function getCities(event) {
     const citySelect = document.querySelector("select[name=city]") //Pegar as cidades
 
     const ufValue = event.target.value
-    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios` //URL de mandeira dinamica, mudou o ufValue irá mudar o url
+
+    //Transformação dos dados:
+    fetch(url)
+    .then( res => res.json() ) //Transformou os dados em json
+    .then( cities => { //Vai retornar os estados
+        for( const city of cities) { //city: cidade de cada cities (cidades de cada estado)
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }        
+    })
 }
 
 document
