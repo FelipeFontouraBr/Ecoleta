542ff62fd33d0a7e6738e760a67f19138d3167de
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-06-10 18:12:33 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,13 @@
 const express = require("express") //Iniciar o servidor, irá pedir o express e salvar nessa variável
 const server = express()//Executar o express
 
+//Configuração das rotas/caminhos da aplicação
+//Rota "Home", página inicial.
+//req: Requisição
+//res: Resposta
+server.get("/", (req, res) => { //Get é um verbo de http 
+    res.send("Olá Felipe")
+})
+
 //ligar o servidor
 server.listen(3000)
