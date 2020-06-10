d3f6f2d51ee830c9187874585f99f1d1ad45113f
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-06-10 18:32:59 +0200

    Created route to index.html

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 const express = require("express") //Iniciar o servidor, irá pedir o express e salvar nessa variável
 const server = express()//Executar o express
 
+//Configuração da pasta public 
+server.use(express.static("public")) //Configuração da pasta public para que apareça as paginas dentro dela
+
+
 //Configuração das rotas/caminhos da aplicação
 //Rota "Home", página inicial.
 //req: Requisição
 //res: Resposta
 server.get("/", (req, res) => { //Get é um verbo de http 
-    res.send("Olá Felipe")
+    res.sendFile(__dirname + "/views/index.html") //dirname é o nome do diretório
 })
 
 //ligar o servidor
