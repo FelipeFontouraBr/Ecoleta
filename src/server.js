d6a89a87239ecace4044f9bb9d6815a35875e583
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-06-10 17:13:55 +0200

    Created const to turn on of the server
    Created const to turn on of the server using port 3000

--- src/server.js
+++ src/server.js
@@ -1,1 +1,5 @@
 const express = require("express") //Iniciar o servidor, irá pedir o express e salvar nessa variável
+const server = express()//Executar o express
+
+//ligar o servidor
+server.listen(3000)
