83b9f1f64b540838d4298def7b53b162c62990a1
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-06-11 17:52:51 +0200

    Added route to "Search-Results"

--- src/server.js
+++ src/server.js
@@ -30,6 +30,10 @@ server.get("/create-point", (req, res) => {
     return res.render("create-point.html") 
 })
 
+//Rota /Search-results
+server.get("/search", (req, res) => { 
+    return res.render("search-results.html") 
+})
 
 //ligar o servidor
 server.listen(3000)
