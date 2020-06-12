96da3f1fcf00aef0ad95ed22313bbbe0431cfa2b
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-06-13 00:37:40 +0200

    Created depend to sqlite3

--- src/database/db.js
+++ src/database/db.js
@@ -1,4 +1,5 @@
 //Configuração do nosso banco de dados
 //Importar a depedencia do sqlite3, e irá retornar um objeto:
+//Dica: uma função dentro de um objeto se chama METODO
 
-const sqlite3 = require("sqlite3").verbose() //Verbose: quero ver uma mensagem no terminal toda vez que acontecer algo
+const sqlite3 = require("sqlite3").verbose() //Verbose: quero ver uma mensagem no terminal toda vez que acontecer algo e retorna um objeto
