c947b6c710e6a73dfe74ad7d66c79f5d94206ea1
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-06-13 00:36:20 +0200

    Created db.js

---
+++ src/database/db.js
@@ -0,0 +1,4 @@
+//Configuração do nosso banco de dados
+//Importar a depedencia do sqlite3, e irá retornar um objeto:
+
+const sqlite3 = require("sqlite3").verbose() //Verbose: quero ver uma mensagem no terminal toda vez que acontecer algo
