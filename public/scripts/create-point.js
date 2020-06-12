2a02af4ce909015c5f24dc211863000ec7381779
FelipeFontouraBr <felipefontouramec@gmail.com>
2020-06-12 23:22:45 +0200

    Update create-point.js

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -9,12 +9,12 @@ function populateUFs() {
     const ufSelect = document.querySelector("select[name=uf]")
 
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-    .then( res => res.json() ) //Transformou os dados em json
-    .then( states => { //Vai retornar os estados
-        for( const state of states ) { 
-            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
-        }        
-    })
+        .then(res => res.json()) //Transformou os dados em json
+        .then(states => { //Vai retornar os estados
+            for (const state of states) {
+                ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+            }
+        })
 }
 
 populateUFs()
@@ -37,15 +37,15 @@ function getCities(event) { //getCities é chamado toda vez que troco o uf (esta
 
     //Transformação dos dados:
     fetch(url)
-    .then( res => res.json() ) //Transformou os dados em json
-    .then( cities => { //Vai retornar as cidades
-        
-        for( const city of cities) { //city: cidade de cada cities (cidades de cada estado)
-            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>` //Nessa lógica que adicionamos uma cidade nova
-        }    
-    //Habilitar o select do campo "cidade":
-    citySelect.disabled = false
-    })
+        .then(res => res.json()) //Transformou os dados em json
+        .then(cities => { //Vai retornar as cidades
+
+            for (const city of cities) { //city: cidade de cada cities (cidades de cada estado)
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>` //Nessa lógica que adicionamos uma cidade nova
+            }
+            //Habilitar o select do campo "cidade":
+            citySelect.disabled = false
+        })
 }
 
 //Adicionamos um ouvidor de evento a apenas um elemento (uf)
@@ -78,17 +78,17 @@ function handleSelectedItem(event) {
 
     const itemId = itemLi.dataset.id//colocamos o dataset.id (para pegar os numeros de id dos itens) dentro de uma variável. Ou seja, quando clico, jogo o id dentro dessa variavel
 
-    
-//Verificar se existem itens selecionados, se sim, pegar os itens selecionados
+
+    //Verificar se existem itens selecionados, se sim, pegar os itens selecionados
     const alreadySelected = selectedItems.findIndex(item => { //findIndex irá receber uma função curta
         const itemFound = item == itemId//Encontrei o item, se o item for igual ao itemId /return é sempre tru or false
         return itemFound
-    }) 
+    })
 
-//Se já estiver selecionado:
-    if( alreadySelected >= 0 ) {
+    //Se já estiver selecionado:
+    if (alreadySelected >= 0) {
         //tirar da seleção:
-        const filteredItems = selectedItems.filter( item => {
+        const filteredItems = selectedItems.filter(item => {
             //Caso retorne false, irá adicionar em um novo array na constante filteredItems:
             const itemIsDifferent = item != itemId
             return itemIsDifferent
@@ -99,9 +99,9 @@ function handleSelectedItem(event) {
     }
     console.log(selectedItems)
 
-//Atualizar o campo escondido com os itens selecionados
-collectesItems.value = selectedItems
+    //Atualizar o campo escondido com os itens selecionados
+    collectesItems.value = selectedItems
 
-    
-    
+
+
 }
